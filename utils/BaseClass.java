ed6e03099384d7366e39ea51c62c415f994ea32e
avinashworks <123383409+avinashworks@users.noreply.github.com>
2025-07-25 10:06:12 +0530

    Create BaseClass.java

---
+++ utils/BaseClass.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+
+    private static WebDriver driver;
+
+    public static void initializeDriver() {
+        WebDriverManager.chromedriver().setup(); // Downloads matching chromedriver
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
